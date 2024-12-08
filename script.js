539a492acc7a6ac98b6581b236fbec65104ed2df
24clarson <93607328+24clarson@users.noreply.github.com>
2024-12-08 11:20:55 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 console.log("Hello World");
+
+const clientId = '142021';
+const redirectUri = '24clarson.github.io/bubbles/';
+const authUrl = `https://www.strava.com/oauth/authorize?client_id=${clientId}&response_type=code&redirect_uri=${redirectUri}&scope=activity:read_all,read_all&approval_prompt=auto`;
+
+document.getElementById('auth').href = authUrl;
+
+// Parse URL parameters
+const params = new URLSearchParams(window.location.search);
+const authorizationCode = params.get('code');
+
+if (authorizationCode) {
+    console.log('Authorization Code:', authorizationCode);
+
+    // Exchange the authorization code for an access token
+    // fetchAccessToken(authorizationCode);
+}
+
