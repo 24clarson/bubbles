fee145f9489d3fb92b7719ee22bc324d0e6f33e2
24clarson <93607328+24clarson@users.noreply.github.com>
2024-12-08 11:04:22 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,1 @@
 console.log("Hello World");
-
-document.getElementById("redirect_link").href = "https://www.strava.com/oauth/authorize?client_id=142021&response_type=code&redirect_uri=https://24clarson.github.io/bubbles/&scope=read_all,activity:read_all&approval_prompt=auto"
